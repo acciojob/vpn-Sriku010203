11942528441cea6709e4f79c1249bddba69a6d03
Srikanth Naik <48707298+Sriku010203@users.noreply.github.com>
2024-07-05 15:25:05 +0530

    Update ServiceProvider.java

--- src/main/java/com/driver/model/ServiceProvider.java
+++ src/main/java/com/driver/model/ServiceProvider.java
@@ -7,28 +7,18 @@ import java.util.List;
 public class ServiceProvider {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
+    private Long id;
     private String name;
 
-    @ManyToOne
-    @JoinColumn
-    Admin admin;
+    @OneToMany(mappedBy = "serviceProvider")
+    private List<Country> countries = new ArrayList<>();
 
-    @OneToMany(mappedBy = "serviceProvider", cascade = CascadeType.ALL)
-    List<Country> countryList;
-
-    @ManyToMany(mappedBy = "serviceProviderList", cascade = CascadeType.ALL)
-    List<User> users;
-
-    @OneToMany(mappedBy = "serviceProvider", cascade = CascadeType.ALL)
-    List<Connection> connectionList;
-
-    public int getId() {
+    // Getters and Setters
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -40,35 +30,11 @@ public class ServiceProvider {
         this.name = name;
     }
 
-    public Admin getAdmin() {
-        return admin;
+    public List<Country> getCountries() {
+        return countries;
     }
 
-    public void setAdmin(Admin admin) {
-        this.admin = admin;
-    }
-
-    public List<Country> getCountryList() {
-        return countryList;
-    }
-
-    public void setCountryList(List<Country> countryList) {
-        this.countryList = countryList;
-    }
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
-
-    public List<Connection> getConnectionList() {
-        return connectionList;
-    }
-
-    public void setConnectionList(List<Connection> connectionList) {
-        this.connectionList = connectionList;
+    public void setCountries(List<Country> countries) {
+        this.countries = countries;
     }
 }
